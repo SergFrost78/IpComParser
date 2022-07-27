db5e85f93fdcbe0cd3b6bef4b93f8047ecb0248a
SergFrost78 <68305305+SergFrost78@users.noreply.github.com>
2022-07-28 01:05:15 +0300

    rebuild with grid layout

--- modules/DateTime/my_dateTime.py
+++ modules/DateTime/my_dateTime.py
@@ -9,26 +9,38 @@ import configparser
 class MyDateTime(QtWidgets.QWidget):
     def __init__(self, path):
         super(MyDateTime, self).__init__()
+        self.grid = None
         self.path = path
         self.readSettings(self.path)
         self.action = None
         self.reload_action = None
         self.context_menu = None
-        self.layout = None
-        self.label2 = None
         self.label = None
+        self.label2 = None
+        self.label3 = None
+        self.label4 = None
 
         self.datetime_data = {
             'text_1': self.current_time(),
             'font_family_1': 'Arial',
-            'font_size_1': 150,
+            'font_size_1': 130,
             'transparency_1': 1,
             'font_color_1': '255, 255, 0',
-            'text_2': f'{self.current_day_of_week()}. {self.current_date()}',
-            'font_family_2': 'Comic Sans MS',
-            'font_size_2': 30,
+            'text_2': self.current_time_secs(),
+            'font_family_2': 'Times New Roman',
+            'font_size_2': 15,
             'transparency_2': 1,
-            'font_color_2': '0, 0, 0',
+            'font_color_2': '255, 255, 0',
+            'text_3': self.current_day_of_week(),
+            'font_family_3': 'Comic Sans MS',
+            'font_size_3': 70,
+            'transparency_3': 1,
+            'font_color_3': '0, 0, 0',
+            'text_4': self.current_date(),
+            'font_family_4': 'Comic Sans MS',
+            'font_size_4': 30,
+            'transparency_4': 1,
+            'font_color_4': '0, 0, 0',
         }
 
         self.hide_show_flag = 1
@@ -56,21 +68,31 @@ class MyDateTime(QtWidgets.QWidget):
     def init_ui(self):
         self.label = QtWidgets.QLabel()
         self.label2 = QtWidgets.QLabel()
+        self.label3 = QtWidgets.QLabel()
+        self.label4 = QtWidgets.QLabel()
 
         self.label.setAutoFillBackground(False)
         self.label.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
         self.label.setWordWrap(False)
 
         self.label2.setAutoFillBackground(False)
-        self.label2.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignRight | QtCore.Qt.AlignTop)
+        self.label2.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
         self.label2.setWordWrap(False)
 
-        self.layout = QtWidgets.QVBoxLayout()
+        self.label3.setAutoFillBackground(False)
+        self.label3.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignRight | QtCore.Qt.AlignTop)
+        self.label3.setWordWrap(False)
 
-        self.layout.addWidget(self.label)
-        self.layout.addWidget(self.label2)
+        self.label4.setAutoFillBackground(False)
+        self.label4.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        self.label4.setWordWrap(False)
 
-        self.setLayout(self.layout)
+        self.grid = QtWidgets.QGridLayout()
+        self.setLayout(self.grid)
+        self.grid.addWidget(self.label, 0, 0)
+        self.grid.addWidget(self.label2, 0, 1)
+        self.grid.addWidget(self.label3, 1, 0)
+        self.grid.addWidget(self.label4, 1, 1, 1, 1)
 
     def reload(self, data):
         self.label.setText(data['text_1'])
@@ -81,6 +103,16 @@ class MyDateTime(QtWidgets.QWidget):
         self.label2.setText(data['text_2'])
         self.label2.setFont(QtGui.QFont(data['font_family_2'], data['font_size_2']))  # Изменить шрифт
         self.label2.setStyleSheet(f"color: rgba({data['font_color_2']}, {data['transparency_2']});")  # Цвет и 
+        # прозрачность
+
+        self.label3.setText(data['text_3'])
+        self.label3.setFont(QtGui.QFont(data['font_family_3'], data['font_size_3']))  # Изменить шрифт
+        self.label3.setStyleSheet(f"color: rgba({data['font_color_3']}, {data['transparency_3']});")  # Цвет и
+        # прозрачность
+
+        self.label4.setText(data['text_4'])
+        self.label4.setFont(QtGui.QFont(data['font_family_4'], data['font_size_4']))  # Изменить шрифт
+        self.label4.setStyleSheet(f"color: rgba({data['font_color_4']}, {data['transparency_4']});")  # Цвет и
         # прозрачность
 
     def saveSettings(self, path):
@@ -145,20 +177,26 @@ class MyDateTime(QtWidgets.QWidget):
         date_now = ':'.join(date_now)
         return date_now
 
-    def current_time(self):
+    def current_time(self):                             # 22:35:44.65465
         data = datetime.datetime.now()
         time_now = str(data.time())
         time_now = str(time_now).split(':')[:2]
         time_now = ':'.join(time_now)
         return time_now
 
+    def current_time_secs(self):
+        data = datetime.datetime.now()
+        time_now = str(data.time())
+        secs = time_now.split(':')[-1].split('.')[0]
+        return secs
+
     def current_day_of_week(self):
-        self.week_day = datetime.datetime.today().weekday()
+        week_day = datetime.datetime.today().weekday()
         week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
-        return week[self.week_day]
+        return week[week_day]
 
     def run(self):
-        time = self.current_time()
-        self.datetime_data['text_1'] = time
-        self.reload(self.datetime_data)
+        self.datetime_data['text_1'] = self.current_time()
+        self.datetime_data['text_2'] = self.current_time_secs()
 
+        self.reload(self.datetime_data)
